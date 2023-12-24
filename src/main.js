cbb87fba384d6caa5360cafba0167caa6bcecbff
luisaapinho <40220114@esmad.ipp.pt>
2023-12-24 17:49:19 +0000

    main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+//Pinia
+import { createPinia } from 'pinia'
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  
+
+const pinia = createPinia()
+// pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
+
 app.use(router)
+
+app.use(vuetify)
 
 app.mount('#app')
