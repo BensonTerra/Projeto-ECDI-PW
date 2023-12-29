258879af45663829ad63304750e7c0626e0b4f6e
luisaapinho <40220114@esmad.ipp.pt>
2023-12-29 16:53:11 +0000

    fixinggg

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,7 @@ const router = createRouter({
     {
       path: "/:pathMatch(.*)*",
       name: "pagenotfound",
-      component: PageNotFoundView,
+      component: PageNotFoundView
     },
 
   ]
