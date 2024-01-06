9efa8f8b51a9289f522cc1102f863c49ae456363
luisaapinho <40220114@esmad.ipp.pt>
2024-01-06 18:35:28 +0000

    fixingLittle things on pageBlockedView

--- src/views/PageBlockedView.vue
+++ src/views/PageBlockedView.vue
@@ -44,10 +44,10 @@
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url(../assets/BlockedBackground.png);
+  background-image: url(../assets/BlockedBackground.png) !important;
   background-size: cover;
   background-position: 50%;
-  z-index: -1; 
+
 }
 
 
